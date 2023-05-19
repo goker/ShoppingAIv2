be253e976b3bdd6eb3f2bb6fc8ef973982924754
Ozzy Aldeny <ozzy.aldeny@gmail.com>
2023-05-18 22:56:35 -0400

    product_descr

--- ShoppingAI.py
+++ ShoppingAI.py
@@ -2,13 +2,48 @@ import streamlit as st
 import json
 from apify_client import ApifyClient
 import openai
+import urllib
 import os
+import re
+import requests
 from langchain.prompts import PromptTemplate
 from langchain.llms import OpenAI,OpenAIChat
 from langchain.chains import LLMChain, SimpleSequentialChain
 
-# Open_AI_Key = os.getenv('OPENAI_API_KEY')
-# APIFY_Key = os.getenv('APIFY_API_KEY')
+Open_AI_Key = os.getenv('OPENAI_API_KEY')
+APIFY_Key = os.getenv('APIFY_API_KEY')
+
+def get_product_details(asin):
+    # set up the request parameters
+    params = {
+    'api_key': 'BAA8AE82B7624776A5DA9E7D78128241',
+    'amazon_domain': 'amazon.com',
+    'asin': asin,
+    'type': 'product'
+    }
+
+    # make the http GET request to ASIN Data API
+    api_result = requests.get('https://api.asindataapi.com/request', params)
+    return api_result.json()
+
+def strip_amazon_link(link):
+    parsed_url = urllib.parse.urlparse(link)
+    scheme = parsed_url.scheme
+    netloc = parsed_url.netloc
+    path = parsed_url.path
+    query_params = urllib.parse.parse_qs(parsed_url.query)
+    
+    # Retrieve the ASIN from the query parameters
+    asin = query_params.get('ASIN', [''])[0]
+
+    # If ASIN is not found in the query parameters, try to extract it from the path
+    if not asin:
+        path = parsed_url.path
+        match = re.search(r'/dp/(\w+)/?', path)
+        if match:
+            asin = match.group(1)
+
+    return {'stripped_url':f"{scheme}://{netloc}{path}",'asin':asin}
 
 def generate_reviews2(text):
     openai.api_key  = st.session_state['OPENAI_API_KEY']
@@ -35,12 +70,11 @@ def scraproduct(product_url):
     run_input = {
         "productUrls": [{ "url": product_url }],
         "maxReviews": 30,
-        "timeout" :60,
         "proxyConfiguration": { "useApifyProxy": True },
         "extendedOutputFunction": "($) => { return {} }",
     }
     # Run the actor and wait for it to finish
-    run = client.actor("junglee/amazon-reviews-scraper").call(run_input=run_input)
+    run = client.actor("junglee/amazon-reviews-scraper").call(run_input=run_input,timeout_secs=60)
     print(run)
     print("*********")
     items_array = [item for item in client.dataset(run["defaultDatasetId"]).iterate_items()]
@@ -83,35 +117,50 @@ def generate_why_buy(text):
     return chain.run(text)
 
 def main():
-    with st.sidebar:
-        openapikey=st.text_input('Open AI API Key')
-        scapperapikey=st.text_input('Scrapper API Key')
-        if st.button("Set") :
-            st.session_state['OPENAI_API_KEY']=openapikey
-            st.session_state['APIFY_API_KEY']=scapperapikey
     
+    if(Open_AI_Key is None or APIFY_Key is None):
+        with st.sidebar:
+            openapikey=st.text_input('Open AI API Key')
+            scapperapikey=st.text_input('Scrapper API Key')
+            if st.button("Set") :
+                st.session_state['OPENAI_API_KEY']=openapikey
+                st.session_state['APIFY_API_KEY']=scapperapikey
+    else:
+        st.session_state['OPENAI_API_KEY']=Open_AI_Key
+        st.session_state['APIFY_API_KEY']=APIFY_Key
     product_url=st.text_input('Paste Amazon Product URL')
     if ( st.button('Get Reviews')):
-        with st.spinner(text="Getting Reviews ..."):
-            items_array=scraproduct(product_url)
-            if len(items_array):
-                st.session_state['reviews']=items_array
-        with st.spinner(text="AI in Process ..."):
-            review_descriptions = "|".join([obj["reviewDescription"] for obj in items_array])
-            response=""
-            if len(review_descriptions) <= 10000:
-                response=generate_reviews2(review_descriptions)
-            else:
-                response=generate_reviews2(review_descriptions[:10000])
-            st.markdown(response)
-        with st.spinner(text="AI in Process ..."):
-            review_descriptions = "|".join([obj["reviewDescription"] for obj in items_array])
-            response=""
-            if len(review_descriptions) <= 10000:
-                response=generate_why_buy(review_descriptions)
-            else:
-                response=generate_why_buy(review_descriptions[:10000])
-            st.markdown(response)
+        [col1,col2]=st.columns([1,1])
+        with col1:
+            with st.spinner(text="Getting Product ..."):
+                stripped_link=strip_amazon_link(product_url)
+                product_json = get_product_details(stripped_link['asin'])
+                st.markdown(product_json['product']['title'])
+                st.markdown(product_json['product']['feature_bullets_flat'])
+                st.image(product_json['product']['images'][0]['link'],use_column_width='auto')
+                st.markdown(product_json['product']['link']+"?tag=pixelitem0a-20")
+        with col2:
+            # card("Product",'',product_json['product']['images'][0],product_json['product']['link'])
+            with st.spinner(text="Getting Reviews ..."):
+                items_array=scraproduct(stripped_link['stripped_url'])
+                if len(items_array):
+                    st.session_state['reviews']=items_array
+            with st.spinner(text="AI in Process ..."):
+                review_descriptions = "|".join([obj["reviewDescription"] for obj in items_array])
+                response=""
+                if len(review_descriptions) <= 10000:
+                    response=generate_reviews2(review_descriptions)
+                else:
+                    response=generate_reviews2(review_descriptions[:10000])
+                st.markdown(response)
+            with st.spinner(text="AI in Process ..."):
+                review_descriptions = "|".join([obj["reviewDescription"] for obj in items_array])
+                response=""
+                if len(review_descriptions) <= 10000:
+                    response=generate_why_buy(review_descriptions)
+                else:
+                    response=generate_why_buy(review_descriptions[:10000])
+                st.markdown(response)
         
 
 if __name__ == "__main__":
